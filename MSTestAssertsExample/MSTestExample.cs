4feda6a9699e6302eb7f717ce5def7790766220e
andreea-qa <andreea.draniceanu@gmail.com>
2025-05-10 16:02:45 +0300

    added common setup for LT

--- MSTestAssertsExample/MSTestExample.cs
+++ MSTestAssertsExample/MSTestExample.cs
@@ -1,36 +1,21 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using SetUp;
 
 namespace MSTestAsserts
 {
     [TestClass]
     public class MSTestExample
     {
-        private static IWebDriver driver;
-        public static string gridURL = "@hub.lambdatest.com/wd/hub";
-        private static readonly string LT_USERNAME = Environment.GetEnvironmentVariable("LT_USERNAME");
-        private static readonly string LT_ACCESS_KEY = Environment.GetEnvironmentVariable("LT_ACCESS_KEY");
-
+        private static IWebDriver driver;        
 
         [TestInitialize]
         public void OpenApplication()
         {
-            ChromeOptions capabilities = new ChromeOptions();
-            capabilities.BrowserVersion = "latest";
-            Dictionary<string, object> ltOptions = new Dictionary<string, object>();
-            ltOptions.Add("username", LT_USERNAME);
-            ltOptions.Add("accessKey", LT_ACCESS_KEY);
-            ltOptions.Add("platformName", "Windows 10");
-            ltOptions.Add("build", "Selenium CSharp");
-            ltOptions.Add("project", "MSTest Asserts");
-            ltOptions.Add("w3c", true);
-            ltOptions.Add("plugin", "c#-mstest");
-            capabilities.AddAdditionalOption("LT:Options", ltOptions);
-            driver = new RemoteWebDriver(new Uri($"https://{LT_USERNAME}:{LT_ACCESS_KEY}{gridURL}"), capabilities);
+            driver = LambdaDriverFactory.CreateDriver(project: "MSTest Asserts");
             driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/");
         }
 
--- NUnitAssertsExample/NUnitExample.cs
+++ NUnitAssertsExample/NUnitExample.cs
@@ -3,32 +3,19 @@ using OpenQA.Selenium.Chrome;
 using SeleniumExtras.WaitHelpers;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Remote;
+using SetUp;
 
 namespace NUnitAsserts
 {
     internal class NUnitExampe
     {
         private static IWebDriver driver;
-        public static string gridURL = "@hub.lambdatest.com/wd/hub";
-        private static readonly string LT_USERNAME = Environment.GetEnvironmentVariable("LT_USERNAME");
-        private static readonly string LT_ACCESS_KEY = Environment.GetEnvironmentVariable("LT_ACCESS_KEY");
-
+        
         [SetUp]
         public void OpenApplication()
         {
-            ChromeOptions capabilities = new ChromeOptions();
-            capabilities.BrowserVersion = "latest";
-            Dictionary<string, object> ltOptions = new Dictionary<string, object>();
-            ltOptions.Add("username", LT_USERNAME);
-            ltOptions.Add("accessKey", LT_ACCESS_KEY);
-            ltOptions.Add("platformName", "Windows 10");
-            ltOptions.Add("build", "Selenium CSharp");
-            ltOptions.Add("project", "NUnit Asserts");
-            ltOptions.Add("w3c", true);
-            ltOptions.Add("plugin", "c#-nunit");
-            capabilities.AddAdditionalOption("LT:Options", ltOptions);
-            driver = new RemoteWebDriver(new Uri($"https://{LT_USERNAME}:{LT_ACCESS_KEY}{gridURL}"), capabilities);
+
+            driver = LambdaDriverFactory.CreateDriver(project: "MSTest Asserts");
             driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/");
         }
 
